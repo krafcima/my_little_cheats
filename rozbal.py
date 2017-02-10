e8dc0d7b34c0c9aed35098ffdff8f5c668d03c90
krafcima <krafcima@gmail.com>
2017-02-10 23:06:16 +0100

    EXTRACT.py
    This is a little script, that its check the directory, where its located and then will unzip every unzipped .zip, i had created this right now, so i need to catch some issues,  
    its in python 3.xx

---
+++ rozbal.py
@@ -0,0 +1,25 @@
+import os
+from zipfile import ZipFile as rar
+def priecinky(c):
+	pole = c
+	res = []
+	for i in range(len(pole)):
+		if '.zip' not in pole[i]:
+			if '.txt' not in pole[i]:
+				if '.py' not in pole[i]:
+					res.append(pole[i])
+	return res
+r = os.listdir(os.getcwd())
+print(r)
+done  = priecinky(r)
+print(done)
+for i in range(len(r)):
+	pom = ''
+	if '.zip' in r[i]:
+		pom = r[i].replace('.zip','') 
+		with rar(os.getcwd()+'\\' +r[i], 'r') as f:
+			f.extractall(os.getcwd()+'\\' +r[i].replace('.zip', ''))
+			done.append(pom)
+for i in range(len(done)):
+	with open(os.getcwd()+'\data.txt', 'a') as f:
+		print('{}.) {}'.format(i,done[i]), file = f)
